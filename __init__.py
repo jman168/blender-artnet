429760b36ce6771c156210ef56aa77189b03241b
Jason <jasonabeckmann@gmail.com>
2021-06-16 19:08:41 -0400

    Basic plugin

---
+++ __init__.py
@@ -0,0 +1,14 @@
+bl_info = {
+    "name": "Blender Art-Net",
+    "description": "A blender plugin for controling lights via the Art-Net protocol.",
+    "author": "Jason Beckmann",
+    "version": (1, 0),
+    "blender": (2, 90, 0),
+    "category": "Lighting",
+}
+
+def register():
+    print("Hello World")
+
+def unregister():
+    print("Goodbye World")
